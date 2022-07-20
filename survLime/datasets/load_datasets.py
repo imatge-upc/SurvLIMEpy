746924b293fc272fda08fd948a0123dc4d6023ed
CarlosHernandezP <crherperez95@gmail.com>
2022-07-20 10:34:13 +0000

    added util functions and dataloading

--- survLime/datasets/load_datasets.py
+++ survLime/datasets/load_datasets.py
@@ -3,12 +3,19 @@ import numpy as np
 
 veteran_path = '/home/carlos.hernandez/PhD/SurvLIME/survLime/datasets/veteran.csv'
 def Loader(dataset_name : str='veterans') -> list([pd.DataFrame, np.ndarray]):
+    """
+    Loads a survival dataset, for now it only loads
+    the veteran dataset
+    """
+
+    feature_columns = ['celltype', 'trt', 'karno', 'diagtime', 'age', 'prior']
+
     df = pd.read_csv(veteran_path) 
-     
+    
     df['status'] = [True if x==1 else False for x in df['status']]
     df['y'] = [(x,y) for x,y in zip(df['status'], df['time'])]
     y = df.pop('y').to_numpy()
-    x = df[['celltype', 'trt', 'karno', 'diagtime', 'age', 'prior']]
+    x = df[feature_columns]
     
     return x, y
     
--- survLime/utils/generic_utils.py
+++ survLime/utils/generic_utils.py
@@ -1,6 +1,55 @@
+from typing import Union
 import sys
 import inspect
 import types
+
+import numpy
+import pandas as pd
+from math import sqrt
+import matplotlib.pyplot as plt
+from torch.nn import Module
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomSurvivalForest
+from sksurv.linear_model import CoxPHSurvivalAnalysis
+
+    
+def compare_survival_times(bb_model : Union[CoxPHSurvivalAnalysis, Module, RandomSurvivalForest],
+                           coefs : numpy.ndarray,
+                           X_train : pd.DataFrame, y_train : numpy.ndarray, X_test : pd.DataFrame):
+    """Trains a Cox model with the coefs obtained with cvxpy and plots the survival 
+       times as well as the 
+    
+    Args:
+        bb_model: model that we want to find the model_interpretable to
+        coefs: values obtained during the convex optimization problem to be used as Cox coefficients
+        X_train: DataFrame with the training data
+        y_train: numpy.ndarray witht the training labels
+        X_test:  DataFrame with the test data
+
+    Returns:
+        Plots
+    """
+    model_interpretable = CoxPHSurvivalAnalysis()
+    model_interpretable.fit(X_train, y_train)
+    model_interpretable.coef_ = coefs
+
+    preds_cox      = bb_model.predict_survival_function(X_test)
+    preds_survlime = model_interpretable.predict_survival_function(X_test)
+
+    preds_cox_y  = numpy.mean([x.y for x in preds_cox], axis=0)
+    preds_surv_y = numpy.mean([x.y for x in preds_survlime], axis=0)
+
+    plt.plot(preds_cox_y, label='CoxPH')
+    plt.plot(preds_surv_y, label='SurvLIME')
+    plt.title('Mean survival time comparison')
+    plt.legend()
+    rmse = sqrt(mean_squared_error(preds_cox_y, preds_surv_y))
+    print(f' RMSE between the two curves is {round(rmse, 3)}')
+
+    if isinstance(bb_model, CoxPHSurvivalAnalysis):
+        df = pd.DataFrame(columns=bb_model.feature_names_in_, 
+                  data=[coefs, bb_model.coef_], index=['SurvLIME','CoxPH'])
+        df.transpose().plot.bar()
 
 
 def has_arg(fn, arg_name):
