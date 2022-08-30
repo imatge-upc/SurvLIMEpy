01727a619489e717c8abeb2793632ebcacbe94b1
cristianpachon <cc.pachon@gmail.com>
2022-08-30 19:29:38 +0200

    Modify steup file

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
-
 from setuptools import setup, find_packages
 
-setup(name='survLime',
-      version='0.1',
-      description='Survival adaptation of the LIME algorithm',
-      package_dir={'': '.'},
-      packages=find_packages(),
-      install_requires=[],
-      test_suite="tests",
-      include_package_data=True
+setup(
+    name='survLime',
+    version='0.0.1',
+    description='Survival adaptation of the LIME algorithm',
+    package_dir={'': '.'},
+    packages=find_packages(),
+    install_requires=['numpy', 'cvxpy', 'sklearn'],
+    extras_require={'dev': ['pytest']},
+    test_suite="tests",
+    include_package_data=True,
 )
-
