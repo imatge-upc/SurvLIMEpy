df4791707e2aa1eb0b9152c3b2212f17d17803b5
CarlosHernandezP <crherperez95@gmail.com>
2022-09-01 13:52:47 +0000

    add tests for real datasets

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -11,37 +11,36 @@ from sksurv.ensemble import RandomSurvivalForest
 from survLime import survlime_tabular
 from survLime.datasets.load_datasets import Loader
 
+
 def test_shape_veterans_preprocessed() -> None:
     loader = Loader(dataset_name='veterans')
     x, _, _ = loader.load_data()
+    assert x.shape == (137, 6)
 
-    assert x.shape == (137,6)
 
 def test_shape_udca_preprocessed() -> None:
     loader = Loader(dataset_name='udca')
     x, _, _ = loader.load_data()
+    assert x.shape == (170, 4)
 
-    assert x.shape == (170,4)
-    
+
 def test_shape_lung_preprocessed() -> None:
     loader = Loader(dataset_name='lung')
     x, _, _ = loader.load_data()
+    assert x.shape == (228, 8)
 
-    assert x.shape == (228,8)
 
 def test_shape_pbc_preprocessed() -> None:
     loader = Loader(dataset_name='pbc')
     x, _, _ = loader.load_data()
+    assert x.shape == (419, 17)
 
-    assert x.shape == (419,17)
-    
+
 def test_shape_vetearns_computed_weights() -> None:
     loader = Loader(dataset_name='veterans')
     x, events, times = loader.load_data()
     train, _, test = loader.preprocess_datasets(x, events, times, random_seed=0)
-    
     b = compute_weights(train, test)
-
     assert len(b) == 9
 
 
@@ -49,43 +48,41 @@ def test_shape_udca_computed_weights() -> None:
     loader = Loader(dataset_name='udca')
     x, events, times = loader.load_data()
     train, _, test = loader.preprocess_datasets(x, events, times, random_seed=0)
-    
     b = compute_weights(train, test)
-
     assert len(b) == 4
+
 
 def test_shape_lung_computed_weights() -> None:
     loader = Loader(dataset_name='lung')
     x, events, times = loader.load_data()
     train, _, test = loader.preprocess_datasets(x, events, times, random_seed=0)
-    
     b = compute_weights(train, test)
-
     assert len(b) == 11
+
 
 def test_shape_pbc_computed_weights() -> None:
     loader = Loader(dataset_name='pbc')
     x, events, times = loader.load_data()
     train, _, test = loader.preprocess_datasets(x, events, times, random_seed=0)
-    
     b = compute_weights(train, test)
     assert len(b) == 22
 
-def compute_weights(train : np.array, test : np.array) -> List[float]:
-        model = CoxPHSurvivalAnalysis(alpha=0.0001)
 
-        model.fit(train[0], train[1])
+def compute_weights(train: np.array, test: np.array) -> List[float]:
+    model = CoxPHSurvivalAnalysis(alpha=0.0001)
 
-        times_to_fill = list(set([x[1] for x in train[1]]))
-        times_to_fill.sort()
+    model.fit(train[0], train[1])
 
-        explainer = survlime_tabular.LimeTabularExplainer(train[0],
-                                                          train[1])
+    times_to_fill = list(set([x[1] for x in train[1]]))
+    times_to_fill.sort()
 
-        num_pat = 1000
-        predict_chf = partial(model.predict_cumulative_hazard_function, return_array=True)
-        test_point = test[0].iloc[0]
-        b, _= explainer.explain_instance(test_point,
-                                         predict_chf, verbose=False, num_samples = num_pat)
-        b = [x[0] for x in b]
-        return b
+    explainer = survlime_tabular.LimeTabularExplainer(train[0], train[1])
+
+    num_pat = 1000
+    predict_chf = partial(model.predict_cumulative_hazard_function, return_array=True)
+    test_point = test[0].iloc[0]
+    b, _ = explainer.explain_instance(
+        test_point, predict_chf, verbose=False, num_samples=num_pat
+    )
+    b = [x[0] for x in b]
+    return b
