a742b8cedb581ca5c59e8914b5f5ae83d7008eff
CarlosHernandezP <crherperez95@gmail.com>
2022-09-14 09:15:03 +0000

    Add model output times to compute weights function

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,43 +1,40 @@
 from typing import List
 
-import pytest
-import random
 import numpy as np
 from functools import partial
 
 from sksurv.linear_model import CoxPHSurvivalAnalysis
-from sksurv.ensemble import RandomSurvivalForest
 
 from survLime import survlime_tabular
 from survLime.datasets.load_datasets import Loader
 
 
 def test_shape_veterans_preprocessed() -> None:
-    loader = Loader(dataset_name='veterans')
+    loader = Loader(dataset_name="veterans")
     x, _, _ = loader.load_data()
     assert x.shape == (137, 6)
 
 
 def test_shape_udca_preprocessed() -> None:
-    loader = Loader(dataset_name='udca')
+    loader = Loader(dataset_name="udca")
     x, _, _ = loader.load_data()
     assert x.shape == (170, 4)
 
 
 def test_shape_lung_preprocessed() -> None:
-    loader = Loader(dataset_name='lung')
+    loader = Loader(dataset_name="lung")
     x, _, _ = loader.load_data()
     assert x.shape == (228, 8)
 
 
 def test_shape_pbc_preprocessed() -> None:
-    loader = Loader(dataset_name='pbc')
+    loader = Loader(dataset_name="pbc")
     x, _, _ = loader.load_data()
     assert x.shape == (419, 17)
 
 
 def test_shape_vetearns_computed_weights() -> None:
-    loader = Loader(dataset_name='veterans')
+    loader = Loader(dataset_name="veterans")
     x, events, times = loader.load_data()
     train, _, test = loader.preprocess_datasets(x, events, times, random_seed=0)
     b = compute_weights(train, test)
@@ -45,7 +42,7 @@ def test_shape_vetearns_computed_weights() -> None:
 
 
 def test_shape_udca_computed_weights() -> None:
-    loader = Loader(dataset_name='udca')
+    loader = Loader(dataset_name="udca")
     x, events, times = loader.load_data()
     train, _, test = loader.preprocess_datasets(x, events, times, random_seed=0)
     b = compute_weights(train, test)
@@ -53,7 +50,7 @@ def test_shape_udca_computed_weights() -> None:
 
 
 def test_shape_lung_computed_weights() -> None:
-    loader = Loader(dataset_name='lung')
+    loader = Loader(dataset_name="lung")
     x, events, times = loader.load_data()
     train, _, test = loader.preprocess_datasets(x, events, times, random_seed=0)
     b = compute_weights(train, test)
@@ -61,7 +58,7 @@ def test_shape_lung_computed_weights() -> None:
 
 
 def test_shape_pbc_computed_weights() -> None:
-    loader = Loader(dataset_name='pbc')
+    loader = Loader(dataset_name="pbc")
     x, events, times = loader.load_data()
     train, _, test = loader.preprocess_datasets(x, events, times, random_seed=0)
     b = compute_weights(train, test)
@@ -82,7 +79,11 @@ def compute_weights(train: np.array, test: np.array) -> List[float]:
     predict_chf = partial(model.predict_cumulative_hazard_function, return_array=True)
     test_point = test[0].iloc[0]
     b, _ = explainer.explain_instance(
-        test_point, predict_chf, verbose=False, num_samples=num_pat
+        test_point,
+        predict_chf,
+        verbose=False,
+        num_samples=num_pat,
+        model_output_times=model.event_times_,
     )
     b = [x[0] for x in b]
     return b
