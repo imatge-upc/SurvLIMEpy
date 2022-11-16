e8236302de78fa451e6e83c6d130e20e8539ce09
CarlosHernandezP <crherperez95@gmail.com>
2022-11-16 13:45:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="survLime",
-    version="0.0.1",
+    name="survlime",
+    version="0.1.0",
     description="Survival adaptation of the LIME algorithm",
     package_dir={"": "."},
     packages=find_packages(),
     install_requires=["numpy", "cvxpy", "sklearn", "pandas"],
     extras_require={"dev": ["pytest"]},
     test_suite="tests",
+    url='https://github.com/imatge-upc/SurvLIME',
     include_package_data=True,
+    author='Carlos Hernández Pérez, Cristian Pachón García',
+    author_email='crherperez95@gmail.com, cc.pachon@gmail.com'
 )
