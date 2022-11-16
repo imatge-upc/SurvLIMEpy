943bf2672da5802ffc3e85492cdcc051e17a95fe
CarlosHernandezP <crherperez95@gmail.com>
2022-11-16 15:33:34 +0000

    Add .csv files

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="survlime",
-    version="0.1.0",
+    version="0.1.8",
     description="Survival adaptation of the LIME algorithm",
-    package_dir={"": "."},
     packages=find_packages(),
-    install_requires=["numpy", "cvxpy", "sklearn", "pandas"],
+    install_requires=["numpy", "cvxpy", "scikit-survival", "scikit-learn", "pandas"],
     extras_require={"dev": ["pytest"]},
     test_suite="tests",
     url='https://github.com/imatge-upc/SurvLIME',
-    include_package_data=True,
+    package_data= {
+    '': ['datasets/*.csv']
+    },
     author='Carlos Hernández Pérez, Cristian Pachón García',
     author_email='crherperez95@gmail.com, cc.pachon@gmail.com'
 )
