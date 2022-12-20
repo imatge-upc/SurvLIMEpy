db800c21444c584ce75d83db8b64f1dedafd1cb1
CarlosHernandezP <crherperez95@gmail.com>
2022-12-20 16:30:21 +0000

    Add new dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.8",
     description="Survival adaptation of the LIME algorithm",
     packages=find_packages(),
-    install_requires=["numpy", "cvxpy", "scikit-survival", "scikit-learn", "pandas"],
+    install_requires=["numpy", "cvxpy", "scikit-survival", "scikit-learn", "pandas","tqdm","seaborn"],
     extras_require={"dev": ["pytest"]},
     test_suite="tests",
     url='https://github.com/imatge-upc/SurvLIME',
